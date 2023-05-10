63b2a8c20a09981394ad6c04e15a36ed4b70af7e
nokori <69152061+ucelery@users.noreply.github.com>
2023-05-11 00:08:28 +0800

    Add Keyboard Manager Script

--- Assets/Scripts/KeyboardManager.cs
+++ Assets/Scripts/KeyboardManager.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyboardManager : MonoBehaviour {
-    
+    public void OnKeyClick(string key) {
+        Debug.Log(key);
+    }
 }
